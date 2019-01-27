fb8abd61e524bd04ef64bf989f88be69317ffdda
GedMarc <ged_marc@hotmail.com>
2019-01-27 10:14:25 +0200

    module-info removal of import statements

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,9 +1,3 @@
-import com.jwebmp.core.services.IPageConfigurator;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
-import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
-import com.jwebmp.plugins.weathericons.WeatherIconsPageConfigurator;
-import com.jwebmp.plugins.weathericons.implementations.WeatherExclusionsModule;
-
 module com.jwebmp.plugins.weathericons {
 	requires com.jwebmp.core;
 	requires java.validation;
@@ -11,9 +5,9 @@ module com.jwebmp.plugins.weathericons {
 
 	exports com.jwebmp.plugins.weathericons;
 
-	provides IPageConfigurator with WeatherIconsPageConfigurator;
-	provides IGuiceScanModuleExclusions with WeatherExclusionsModule;
-	provides IGuiceScanJarExclusions with WeatherExclusionsModule;
+	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.weathericons.WeatherIconsPageConfigurator;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.weathericons.implementations.WeatherExclusionsModule;
+	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.weathericons.implementations.WeatherExclusionsModule;
 
 	opens com.jwebmp.plugins.weathericons to com.google.guice, com.jwebmp.core;
 }
