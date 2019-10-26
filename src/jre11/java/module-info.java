9e0457e7ee6804e63085d74442361ed49dbb8852
GedMarc <ged_marc@hotmail.com>
2019-10-26 02:35:34 +0200

    Guiced EE Migration to its own space

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -1,13 +1,13 @@
 module com.jwebmp.plugins.weathericons {
 	requires com.jwebmp.core;
 	requires java.validation;
-	requires com.jwebmp.guicedinjection;
+	requires com.guicedee.guicedinjection;
 
 	exports com.jwebmp.plugins.weathericons;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.weathericons.WeatherIconsPageConfigurator;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.weathericons.implementations.WeatherExclusionsModule;
-	provides com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.weathericons.implementations.WeatherExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.weathericons.implementations.WeatherExclusionsModule;
+	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.weathericons.implementations.WeatherExclusionsModule;
 
 	opens com.jwebmp.plugins.weathericons to com.google.guice, com.jwebmp.core;
 }
